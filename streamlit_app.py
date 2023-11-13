dd4576b147a93bbd9094a482cf3742f4e03a9762
pbeaudoin125 <64977819+pbeaudoin125@users.noreply.github.com>
2023-11-13 11:45:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.header('Sommaire par paie')
 # temp write the dataframe to the page so we can see what we're working with
 # streamlit.write(df)
 
-chart_data_agg = df.groupby("Paie")["Valeur"].sum()
+chart_data_agg = df.groupby("Paie")["Valeur"].agg(['sum','count'])
 streamlit.write(chart_data_agg)
 
 # col = streamlit.multiselect("Select any column", chart_data_agg.columns)
