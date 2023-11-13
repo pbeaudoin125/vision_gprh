a2cea52921cc9a35c1421cdad1dc3cce5f16eba9
pbeaudoin125 <64977819+pbeaudoin125@users.noreply.github.com>
2023-11-13 11:51:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,24 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 # run a snowflake query and put it all in a var called results
-# my_cur.execute("select pay_desc, count(distinct directorat_desc) as nb_directorats, sum(bank_value) as valeur_banque from fact_bank group by pay_desc order by pay_desc;")
 my_cur.execute("select pay_desc, bank_value from fact_bank;")
 results = my_cur.fetchall()
 
 # put the dafta into a dataframe
 df = pandas.DataFrame(results, columns=["Paie", "Valeur"])
 
-streamlit.header('Sommaire par paie')
-
 # temp write the dataframe to the page so we can see what we're working with
 # streamlit.write(df)
 
 table_data = df.groupby("Paie")["Valeur"].agg(['sum','count'])
-chart_data = df.groupby("Paie")["Valeur"].sum()
 
+streamlit.header('Sommaire par paie')
 streamlit.write(table_data)
 
-# streamlit.bar_chart(df, x="Paie", y="Valeur")
+my_cur.execute("select directorat_desc, bank_value from fact_bank;")
+results = my_cur.fetchall()
+df = pandas.DataFrame(results, columns=["Paie", "Valeur"])
+chart_data = df.groupby("Paie")["Valeur"].sum()
+
+streamlit.header('Sommaire par directorat')
 streamlit.bar_chart(chart_data)
