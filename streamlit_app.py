3725a08c127593da42b5b60aea6900d6aaa84a8e
pbeaudoin125 <64977819+pbeaudoin125@users.noreply.github.com>
2023-11-13 10:13:25 -0500

    Update streamlit_app.py
    added query and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,16 @@
 import streamlit
+import pandas
+import snowflake.connector
 
 streamlit.title('Banques de vacances')
+
+# connect to snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+# run a snowflake query and put it all in a var called results
+my_cur.execute("select * from fact_bank")
+results = my_cur.fetchall()
+
+# put the dafta into a dataframe
+df = pandas.DataFrame(results)
