217a131d810a8e5c0cff1052ec2e61af71d2e383
pbeaudoin125 <64977819+pbeaudoin125@users.noreply.github.com>
2023-11-13 11:10:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,5 @@ streamlit.header('Sommaire par paie')
 chart_data_agg = df.groupby("Paie")["Valeur"].sum()
 streamlit.write(chart_data_agg)
 
-streamlit.bar_chart(df, x="Paie", y="Valeur")
+# streamlit.bar_chart(df, x="Paie", y="Valeur")
+# streamlit.bar_chart(chart_data_agg, x="Paie", y="Valeur")
