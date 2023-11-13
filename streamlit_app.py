2399df593fe4d45a8902f6a2e2e30e2d16c00ec6
pbeaudoin125 <64977819+pbeaudoin125@users.noreply.github.com>
2023-11-13 10:22:17 -0500

    Update streamlit_app.py
    modified query

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,20 @@ import streamlit
 import pandas
 import snowflake.connector
 
-streamlit.title('Banques de vacances')
+streamlit.title('Banques de vacances - Client 270')
 
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 # run a snowflake query and put it all in a var called results
-my_cur.execute("select * from fact_bank")
+my_cur.execute("select pay_desc, count(distinct directorat_desc) as nb_directorats, sum(bank_value) as valeur_banque from fact_bank group by pay_desc order by pay_desc;")
 results = my_cur.fetchall()
 
 # put the dafta into a dataframe
 df = pandas.DataFrame(results)
+
+streamlit.header('Sommaire par paie')
 
 # temp write the dataframe to the page so we can see what we're working with
 streamlit.write(df)
